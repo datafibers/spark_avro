510bd3c307764096eb598c3632309365e9dca032
Will <willddy@gmail.com>
2017-04-04 14:42:22 -0400

    added

---
+++ src/main/java/org/sparkexample/SparkColumnFilterAvroProducer.java
@@ -0,0 +1,92 @@
+package org.sparkexample;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import org.apache.avro.Schema;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SQLContext;
+import org.apache.spark.sql.types.StructType;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.file.Paths;
+
+public class SparkColumnFilterAvroProducer implements Serializable {
+
+    public static void main(String[] args) {
+        // Run this only in Windows
+        if (System.getProperty("os.name").toUpperCase().contains("WINDOWS"))
+            System.setProperty("hadoop.home.dir", Paths.get("src/main/resources").toAbsolutePath().toString());
+
+        SparkColumnFilterAvroProducer wholeTextFiles = new SparkColumnFilterAvroProducer();
+        wholeTextFiles.run(
+                "\\|",
+                Paths.get("src/main/resources/data").toAbsolutePath().toString(),
+                Paths.get("src/main/resources/schema", "twitter.avsc").toAbsolutePath().toString(),
+                Paths.get("src/main/resources/avro").toAbsolutePath().toString()
+        );
+    }
+
+    private void run(String delimiter, String path, String schemaPath, String avroPath) {
+
+
+        try {
+            final Schema avroSchema = new Schema.Parser().parse(Files.toString(new File(schemaPath), Charsets.UTF_8));
+
+            SparkConf sparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkColumnFilterAvroProducer");
+
+            JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf);
+            JavaRDD<String> content =
+                    javaSparkContext.textFile(path);
+
+            JavaRDD<String> contentFiltered = content.filter(new Function<String, Boolean>() {
+                @Override
+                public Boolean call(String content) throws Exception {
+                    String[] line = content.split(delimiter);
+                    return !(line[0].equalsIgnoreCase("H") || line[0].equalsIgnoreCase("T"));
+                }
+            });
+
+            JavaRDD<Row> rowRDD = contentFiltered
+                    .map(new Function<String, String[]>() {
+                        @Override
+                        public String[] call(String line) throws Exception {
+                            return line.split(delimiter);
+                        }
+                    })
+                    .map(new Function<String[], Row>() {
+                        @Override
+                        public Row call(String[] line) throws Exception {
+                            return RowFactory.create(
+                                    SparkEngineUtility.structDecodingFromLine(
+                                            line,
+                                            Files.toString(new File(schemaPath), Charsets.UTF_8)));
+                        }
+                    });
+
+
+            SQLContext sqlContext = new SQLContext(javaSparkContext);
+
+            StructType schema = SparkEngineUtility.structTypeForSchema(avroSchema);
+
+            sqlContext.createDataFrame(rowRDD, schema)
+                    .coalesce(1)
+                    .write()
+                    .mode("overwrite")
+                    .format("com.databricks.spark.avro")
+                    .save(avroPath)
+            ;
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+
+    }
+}
--- src/main/java/org/sparkexample/SparkLineFilterAvroProducer.java
+++ src/main/java/org/sparkexample/SparkLineFilterAvroProducer.java
@@ -4,59 +4,81 @@ import com.google.common.base.Charsets;
 import com.google.common.io.Files;
 import org.apache.avro.Schema;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.types.StructType;
+import scala.Tuple2;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class SparkAvroProducer implements Serializable {
+public class SparkLineFilterAvroProducer implements Serializable {
 
     public static void main(String[] args) {
         // Run this only in Windows
-        if(System.getProperty("os.name").toUpperCase().contains("WINDOWS"))
+        if (System.getProperty("os.name").toUpperCase().contains("WINDOWS"))
             System.setProperty("hadoop.home.dir", Paths.get("src/main/resources").toAbsolutePath().toString());
 
-        SparkAvroProducer wholeTextFiles = new SparkAvroProducer();
+        SparkLineFilterAvroProducer wholeTextFiles = new SparkLineFilterAvroProducer();
         wholeTextFiles.run(
+                "\\|",
+                2, 1,
                 Paths.get("src/main/resources/data").toAbsolutePath().toString(),
                 Paths.get("src/main/resources/schema", "twitter.avsc").toAbsolutePath().toString(),
                 Paths.get("src/main/resources/avro").toAbsolutePath().toString()
         );
     }
 
-    private void run(String path, String schemaPath, String avroPath) {
-
+    private void run(String delimiter, int headerRowRemoved, int trailerRowRemoved,
+                     String sourcePath, String schemaPath, String avroPath) {
 
         try {
             final Schema avroSchema = new Schema.Parser().parse(Files.toString(new File(schemaPath), Charsets.UTF_8));
 
-            SparkConf sparkConf = new SparkConf().setMaster("local[*]").setAppName("whole text files");
+            SparkConf sparkConf = new SparkConf().setMaster("local[*]").setAppName("SparkLineFilterAvroProducer");;
 
             JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf);
-            JavaRDD<String> content =
-                    javaSparkContext.textFile(path);
+            JavaPairRDD<String, String> fileNameContentsRDD = javaSparkContext.wholeTextFiles(sourcePath);
 
-            JavaRDD<String> contentFiltered = content.filter(new Function<String, Boolean>() {
-                @Override
-                public Boolean call(String content) throws Exception {
-                    return !(content.split("\\|")[0].equalsIgnoreCase("H")
-                            || content.split("\\|")[0].equalsIgnoreCase("T"));
-                }
-            });
+            JavaRDD<String> contentFiltered = fileNameContentsRDD
+                    .flatMap(new FlatMapFunction<Tuple2<String, String>, String>() {
+                        public Iterable<String> call(Tuple2<String, String> file) throws Exception {
+                            String content = file._2();
+                            ArrayList<String> contentArrayList = new ArrayList<>(Arrays.asList(
+                                    content.split(System.getProperty("line.separator"))
+                            ));
+
+                            if(headerRowRemoved + trailerRowRemoved < contentArrayList.size()) {
+                                for(int i = 0; i < headerRowRemoved; i++) {
+                                    contentArrayList.remove(0);
+                                }
+                                for(int i = 0; i < trailerRowRemoved; i++) {
+                                    contentArrayList.remove(contentArrayList.size() - 1);
+                                }
+                            }
+
+                            String[] simpleArray = new String[contentArrayList.size()];
+                            simpleArray = contentArrayList.toArray(simpleArray);
+                            return Arrays.asList(contentArrayList.toArray(simpleArray));
+                        }
+                    });
+
 
             JavaRDD<Row> rowRDD = contentFiltered
                     .map(new Function<String, String[]>() {
                         @Override
                         public String[] call(String line) throws Exception {
-                            return line.split("\\|");
+                            return line.split(delimiter);
                         }
                     })
                     .map(new Function<String[], Row>() {
@@ -68,7 +90,6 @@ public class SparkAvroProducer implements Serializable {
                                             Files.toString(new File(schemaPath), Charsets.UTF_8)));
                         }
                     });
-
 
             SQLContext sqlContext = new SQLContext(javaSparkContext);
 
@@ -82,10 +103,14 @@ public class SparkAvroProducer implements Serializable {
                     .save(avroPath)
             ;
 
-        }catch (IOException ioe) {
+        } catch (IOException ioe) {
             ioe.printStackTrace();
         }
 
+    }
+
+    private void run(String delimiter, String sourcePath, String schemaPath, String avroPath) {
+        run(delimiter, 0, 0, sourcePath, schemaPath, avroPath);
 
     }
 }
